9cff8ffb8bf6cddf93c617f2db54808cdbb20c59
rokyloo <78405453+rokyloo@users.noreply.github.com>
2021-04-24 15:10:19 +0300

    PROG-39, PROG-41
    Pridėjau versijos ikoną ir ataskaitos formavimo mygtuką.

--- Pardavimu_administravimas/Form1.Designer.cs
+++ Pardavimu_administravimas/Form1.Designer.cs
@@ -49,7 +49,11 @@ namespace Pardavimu_administravimas
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.Country = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pasirinkimasAgentura
@@ -220,11 +224,42 @@ namespace Pardavimu_administravimas
             this.Country.TabIndex = 8;
             this.Country.Text = "Pasirinkite kryptį";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Pardavimu_administravimas.Properties.Resources.planeIconv2;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 418);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(20, 19);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 421);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "0.0.1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 235);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 41);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Suformuoti ataskaitą";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.Country);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.textBox1);
@@ -239,6 +274,7 @@ namespace Pardavimu_administravimas
             this.Text = "Pardavimų administravimas";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -266,6 +302,9 @@ namespace Pardavimu_administravimas
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Label Country;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
